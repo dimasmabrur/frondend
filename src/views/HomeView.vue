8a3b97ca7887b2ba713a188f24c48d13828925c3
dimasmabrur <dimasmabrur15@gmail.com>
2025-06-21 09:12:35 +0700

    membuat halaman home

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,23 @@
-<template></template>
+<template>
+  <div class="container mt-4">
+    <h2>Daftar Mobil</h2>
+    <div class="row">
+      <MobilCard v-for="mobil in mobils" :key="mobil.id" :mobil="mobil" />
+    </div>
+  </div>
+</template>
 
 <script>
-export default {};
-</script>
+import MobilCard from "../components/MobilCard.vue";
 
-<style></style>
+export default {
+  components: { MobilCard },
+  data() {
+    return { mobils: [] };
+  },
+  async mounted() {
+    const res = await fetch("https://rental-backend.<subdomain>.workers.dev/mobil");
+    this.mobils = await res.json();
+  },
+};
+</script>
